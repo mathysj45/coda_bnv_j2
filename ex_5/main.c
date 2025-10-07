e12a53a4aa2aa5b89f2bfda420c98e3d5b9f0b88
mathys <mathys.j45@gmail.com>
2025-10-07 12:08:03 +0000

    exercice 5

---
+++ ex_5/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	float x = 12;
+	float y = 7;
+	float resultat = x + y / 2;
+	printf("La moyenne de 12 et 7 est egale a %.2f\n", resultat);
+	exit(0);
+}
